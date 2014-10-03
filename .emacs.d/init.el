4208fca2350caf474b15c3c17a9ec4e8e053b9b7
Kristian Kaa <kaakristian@gmail.com>
2014-10-03 13:26:33 +0200

    Added auto complete

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,7 @@
 (require 'drupal-mode)
 (require 'web-mode)
 
+; Set modes
 (autoload 'php-mode "php-mode" "Major mode for editing php code." t)
 (add-to-list 'auto-mode-alist '("\\.php$" . drupal-mode))
 (add-to-list 'auto-mode-alist '("\\.inc$" . drupal-mode))
@@ -12,6 +13,15 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl.php$" . web-mode))
 
+; Auto complete
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
+(ac-config-default)
+(add-to-list 'ac-modes 'drupal-mode)
+
+; Path to the docs for php
+(setq php-manual-path "~/.emacs.d/php-chunked-xhtml")
+
 (load-theme 'wombat)
 
 ; Set backup directory
@@ -19,3 +29,35 @@
 
 ; Add line numbers
 (global-linum-mode t)
+
+; Indention of arrays
+(add-hook 'php-mode-hook (lambda ()
+    (defun ywb-php-lineup-arglist-intro (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (+ (current-column) c-basic-offset))))
+    (defun ywb-php-lineup-arglist-close (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (current-column))))
+    (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
+    (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)))
+
+; Closure indentation.
+(defun unindent-closure ()
+  "Fix php-mode indent for closures"
+  (let ((syntax (mapcar 'car c-syntactic-context)))
+    (if (and (member 'arglist-cont-nonempty syntax)
+             (or
+              (member 'statement-block-intro syntax)
+              (member 'brace-list-intro syntax)
+              (member 'brace-list-close syntax)
+              (member 'block-close syntax)))
+       (save-excursion
+          (beginning-of-line)
+          (delete-char (* (count 'arglist-cont-nonempty syntax)
+                          c-basic-offset))) )))
+
+(add-hook 'php-mode-hook
+          (lambda ()
+            (add-hook 'c-special-indent-hook 'unindent-closure)))
