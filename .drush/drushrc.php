cc81b1d4784599fec09e11b80a49cc99ca5b9739
Kristian Kaa <kaakristian@gmail.com>
2016-09-28 09:56:05 +0200

    Removed unused Drushrc

--- .drush/drushrc.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Drush alias CA for clearing all cache.
-$options['shell-aliases']['ca'] = 'cache-clear all';
-
-// Drush alias dump to dump a database into the current dir.
-$options['result-file'] = '@DATABASE_@DATE.sql';
-$options['structure-tables']['common'] = array('cache', 'cache_*', 'history', 'search_*', 'sessions', 'watchdog');
-$options['shell-aliases']['dump'] = 'sql-dump –skip-tables-key=common';
