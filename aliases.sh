c3107e61f9f6af7d45c1598a3bf52c7437b7c3ac
Kristian Kaa <kristiankaa@Kristians-iMac.local>
2017-05-24 19:08:48 +0200

    Added aliases

--- aliases.sh
+++ aliases.sh
@@ -10,4 +10,12 @@ alias ...="cd ../.."
 alias l="ls -lha"
 alias w="cd ~/www"
 
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
+git config --global alias.type 'cat-file -t'
+git config --global alias.dump 'cat-file -p'
+
 export PS1="\w/ "
