7542234a3cafc79ab4f83322f6fe89c6147dd0ed
Kristian Kaa <kaakristian@gmail.com>
2018-02-13 21:08:06 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -2,9 +2,6 @@ if [ -f ~/git-completion.bash ]; then
     . ~/git-completion.bash
 fi
 
-# Shortcuts
-alias drush='docker run -v `pwd`:/app --rm drush/drush:8'
-
 alias ..="cd .."
 alias ...="cd ../.."
 alias l="ls -lha"
