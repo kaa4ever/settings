1f45573178317e57662f089e0da4954ebf1cacdb
Kristian Kaa <kaakristian@gmail.com>
2019-10-30 10:20:26 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -24,5 +24,6 @@ git config --global alias.br branch
 git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
 git config --global alias.type 'cat-file -t'
 git config --global alias.dump 'cat-file -p'
+git config --global alias.cleanup 'remote prune origin'
 
 export PS1="\w/ "
